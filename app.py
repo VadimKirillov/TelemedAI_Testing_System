f5028c0dd15d9f8b1c37727f467a03d8244623d3
Ver33d <sergei33dd@mail.ru>
2024-03-30 12:53:49 +0300

    add database.config

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template, request
+from models import db
+from database import create_tables_if_not_exist
+import json
+import os
 
 app = Flask(__name__)
+
+# Загрузка конфигурации из файла
+with open('config/config.json', 'r') as f:
+    config = json.load(f)
+
+# Настройка подключения к базе данных
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}".format(
+    **config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Инициализация экземпляра SQLAlchemy
+db.init_app(app)
+
+# Создание таблиц, если они не существуют
+if not os.path.exists("config/initialized"):
+    with app.app_context():
+        create_tables_if_not_exist()
+        open("config/initialized", "w").close()
 
 
 # Главная страница
---
+++ database.py
@@ -0,0 +1,13 @@
+from models import db
+from sqlalchemy import inspect
+
+
+def create_tables_if_not_exist():
+    connection = db.engine.connect()
+    inspector = inspect(connection)
+    if not inspector.has_table('question') or \
+            not inspector.has_table('option') or \
+            not inspector.has_table('test') or \
+            not inspector.has_table('user_attempt'):
+        db.create_all()
+        print("Таблицы созданы.")
