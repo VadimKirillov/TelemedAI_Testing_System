967eb6607eb7088b28e31ce3fffe63a098ea43d0
Ver33d <sergei33dd@mail.ru>
2024-03-30 11:59:04 +0300

    add pages and top menu

--- app.py
+++ app.py
@@ -3,9 +3,34 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+# Главная страница
 @app.route("/")
 def base():
     return render_template("base.html")
+
+
+# Страница с тестами
+@app.route("/tests")
+def tests():
+    return render_template("tests.html")
+
+
+# Страница с вопросами
+@app.route("/question")
+def question():
+    return render_template("question.html")
+
+
+# Страница с созданием вопросов
+@app.route("/create_tests")
+def create_tests():
+    return render_template("create_tests.html")
+
+
+# Страница со статистикой
+@app.route("/statistics")
+def statistics():
+    return render_template("statistics.html")
 
 
 if __name__ == "__main__":
