b4ee78aea5d9f50c0622e2b042c8203b84bfe48b
Ver33d <sergei33dd@mail.ru>
2024-03-30 14:15:54 +0300

    add database questions

--- models.py
+++ models.py
@@ -3,40 +3,46 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+# Таблица с вопросами
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.String(255), nullable=True)
-    options = db.relationship('Option', backref='question', lazy='dynamic')
-    test_id = db.Column(db.Integer, db.ForeignKey('test.id'), nullable=False)
+    difficulty = db.Column(db.Integer, db.ForeignKey('difficult.id'), nullable=False)
+    modality = db.Column(db.Integer, db.ForeignKey('modal.id'), nullable=False)
+    target_body = db.Column(db.Integer, db.ForeignKey('target.id'), nullable=False)
 
 
-class Option(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
-    is_correct = db.Column(db.Boolean, nullable=False, default=False)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
-
-
-class Test(db.Model):
+class Difficult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    questions = db.relationship('Question', backref='test', lazy='dynamic')
+    rank = db.Column(db.Integer, nullable=False)
 
 
-class UserAttempt(db.Model):
+class Modal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    test_id = db.Column(db.Integer, db.ForeignKey('test.id'), nullable=False)
-    score = db.Column(db.Integer, nullable=False, default=0)
-    timestamp = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
-    test = db.relationship('Test', backref='attempts')
+    name = db.Column(db.String(255), nullable=False)
+
+
+class Target(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    is_correct = db.Column(db.Boolean, nullable=False, default=False)
+    id_question = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
+
+
 
 def create_tables_if_not_exist():
     engine = db.engine()
     if not engine.dialect.has_table(engine, 'question') or \
-            not engine.dialect.has_table(engine, 'option') or \
-            not engine.dialect.has_table(engine, 'test') or \
-            not engine.dialect.has_table(engine, 'user_attempt'):
+            not engine.dialect.has_table(engine, 'difficult') or \
+            not engine.dialect.has_table(engine, 'modal') or \
+            not engine.dialect.has_table(engine, 'target') or \
+            not engine.dialect.has_table(engine, 'answer'):
         db.create_all()
         print("Таблицы созданы.")
